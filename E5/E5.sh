17e2a03b7219592e3269c15916d121e6753431d0
AxelPerales <89339618+AxelPerales@users.noreply.github.com>
2021-09-20 19:10:16 -0500

    Update E5.sh

--- E5/E5.sh
+++ E5/E5.sh
@@ -1,29 +1,23 @@
 #!/bin/bash
-
-#Irvin Martínez González
-#Darian Michelle Orona Aguilar
-
+#Axel Manuel Perales Teofilo
 echo 'Ingrese la API key:'
 read -rs APIKEY
 
 API-Request() {
-#API key: cfccfca8a55b497f9f3c5b22a9cd132a
 while read i; do 
     printf "Correo: $i\n"
-    curl -H "hibp-api-key:$APIKEY" -H "user-agent: Studies" -isS "https://haveibeenpwned.com/api/v3/breachedaccount/$i" > test.txt
+    curl -H "hibp-api-key:$APIKEY" -H "user-agent: Studies" -isS "https://haveibeenpwned.com/api/v2/breachedaccount/$i" > info.txt
 
-    if grep -q "HTTP/2 404" test.txt; then
+    if grep -q "HTTP/2 404" info.txt; then
         echo "Tu cuenta no ha sido vulnerada."
-    #elif grep -q "HTTP/2 401" test.txt; then
-    #    echo "Ha ocurrido un error con el request hacía la API."
     else 
         echo "Tu cuenta ha sido vulnerada en los siguientes servicios: "
-        grep "Name" test.txt
+        grep "Name" info.txt
     fi
 
     printf "\n";
-done < correos.txt
+done < info.txt
 }
 
 API-Request
-rm test.txt
+rm info.txt
